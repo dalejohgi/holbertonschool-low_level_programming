ce38e14ee0b4aaa18ea2998c08c6526ae0877124
David Hincapie <1508@holbertonschool.com>
2020-02-06 22:57:03 +0000

    WTF

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char az = 'a';
+	char AZ = 'A';
+
+	while (az <= 'z')
+{
+	putchar(az);
+	az++;
+}
+
+	while (AZ <= 'Z')
+
+{
+	putchar('\n');
+}
+return (0);
+}
