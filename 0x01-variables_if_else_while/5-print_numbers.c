98d42b77b69306ae87a8b325df281e6a572b3543
David Hincapie <1508@holbertonschool.com>
2020-02-06 23:51:28 +0000

    Make it real dude

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int az = '0';
+
+	while (az <= '9')
+{
+	putchar(az);
+	az++;
+}
+{
+	putchar('\n');
+}
+return (0);
+}
