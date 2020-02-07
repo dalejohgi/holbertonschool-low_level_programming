97b05379e70ed229db4f2a8246c25d3253ed5d0b
David Hincapie <1508@holbertonschool.com>
2020-02-07 00:04:00 +0000

    reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char za = 'z';
+
+	while (za >= 'a')
+{
+	putchar(za);
+	za--;
+}
+{
+	putchar('\n');
+}
+return (0);
+}
