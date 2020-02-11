dd7a5cb7d1208c032b9de3ebf144bce478fa9859
David Hincapie <1508@holbertonschool.com>
2020-02-11 14:47:08 +0000

    Updates

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -8,14 +8,14 @@ int main(void)
 {
 	int i = 0;
 	char c;
-	char s[]="Holberton\n";
-	
+	char s[] = "Holberton\n";
+
 	while (i <= 9)
 		{
-		c=s[i];
+		c = s[i];
 
 		_putchar(c);
 		i++;
 		}
-return(0);
+return (0);
 }
