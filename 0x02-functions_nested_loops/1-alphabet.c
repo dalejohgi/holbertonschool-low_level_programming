689300352368073989f1a0627a9f30004229d5d7
David Hincapie <1508@holbertonschool.com>
2020-02-11 15:53:39 +0000

    Nice job

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+ * print_alphabet - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+void print_alphabet(void)
+{
+	int i = 0;
+	char s[] = "abcdefghijklmnopqrstuvwxyz";
+
+	while (i <= 26)
+	{
+		_putchar(s[i]);
+		i++;
+	}
+_putchar('\n');
+}
