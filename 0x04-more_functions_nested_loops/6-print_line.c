77eeea5ed01b001c83a9c85a2aa60724d2fe0bd0
David Hincapie <1508@holbertonschool.com>
2020-02-13 21:29:10 +0000

    Print line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+/**
+ *print_line - Function to print lines
+ *@n: parameter
+ *
+ */
+void print_line(int n)
+{
+	int i;
+
+	if (n == 0)
+	{
+	_putchar('\n');
+	}
+	else
+	{
+	i = 1;
+	while (i <= n)
+	{
+	_putchar('_');
+	i++;
+	}
+	_putchar('\n');
+	}
+}
