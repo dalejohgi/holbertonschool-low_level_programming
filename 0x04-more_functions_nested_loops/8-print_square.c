3ec71ae9c6ab4becb4cb6321b9caec71ac79ef09
David Hincapie <1508@holbertonschool.com>
2020-02-14 03:09:10 +0000

    Grandee

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+/**
+ *print_diagonal - Function to print o to 9 numbers
+ *@n: variable
+ *
+ */
+void print_square(int size)
+{
+	int a;
+	int b;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (a = 0; a <= size; a++)
+		{
+			for (b = 0; b <= size; b++)
+			{
+				_putchar('#');
+			}
+		_putchar('\n');
+		}
+	}
+}
