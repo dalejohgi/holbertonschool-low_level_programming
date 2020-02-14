d0b2d26bcf49a2a8a742651d538b1b7a864fe520
David Hincapie <1508@holbertonschool.com>
2020-02-14 02:52:46 +0000

    Vamos papa

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+/**
+ *print_diagonal - Function to print o to 9 numbers
+ *@n: variable
+ *
+ */
+void print_diagonal(int n)
+{
+	int a;
+	int b;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (a = 1; a <= n; a++)
+		{
+			for (b = 0; b < a; b++)
+			{
+				_putchar(' ');
+			}
+		_putchar('\\');
+		_putchar('\n');
+		}
+	}
+}
