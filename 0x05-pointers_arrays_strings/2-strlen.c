356e3bf2c8e6e0e9369c2601f257bc6398bcc2f2
David Hincapie <1508@holbertonschool.com>
2020-02-18 19:37:07 +0000

    Ajaaaa

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,19 +1,19 @@
 #include "holberton.h"
 /**
- *
- *
- *
+ *_strlen - Function get the length of a string
+ *Return: the length of the string
+ *@s: Pointer
  */
-int _strlen(char *suputamadre)
+int _strlen(char *s)
 {
 	int i = 0;
 
-	while(*suputamadre != '\0')
+	while (*s != '\0')
 	{
 	i++;
 	s++;
 	}
-	
 
-return(i);
+
+return (i);
 }
