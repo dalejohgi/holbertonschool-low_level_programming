fcb7063d8a3fa9d5fdb6911d4573310cac7fc6c5
David Hincapie <1508@holbertonschool.com>
2020-02-20 22:06:55 +0000

    To compare

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,34 @@
+#include "holberton.h"
+/**
+ *_strcmp - Function that compares two strings
+ *@s1: String 1
+ *@s2: String 2
+ *Return: f
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int i = 0;
+	int n;
+
+	while (s1[i] != '\0')
+	{
+		i++;
+	}
+	n = 0;
+	while (s2[n] != '\0')
+	{
+		n++;
+	}
+	if (i < n)
+	{
+		return (-15);
+	}
+	else if (i > n)
+	{
+		return (15);
+	}
+	else
+	{
+		return (0);
+	}
+}
