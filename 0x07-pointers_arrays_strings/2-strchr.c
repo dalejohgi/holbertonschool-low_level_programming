2523e3e212d3ec0b339d94397944bbe8c18c3421
David Hincapie <1508@holbertonschool.com>
2020-02-24 19:16:43 +0000

    Find de character

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,12 +1,21 @@
 #include "holberton.h"
 /**
- *
- *
- *
- *
+ *_strchr - Function to find a character
+ *@s: pointer to s
+ *@c: Character to find
+ *Return: Pointer to c
  *
  */
 char *_strchr(char *s, char c)
 {
-	
+
+	while (*s != '\0')
+	{
+		if (*s == c)
+		{
+			return (s);
+		}
+	s++;
+	}
+return (0);
 }
