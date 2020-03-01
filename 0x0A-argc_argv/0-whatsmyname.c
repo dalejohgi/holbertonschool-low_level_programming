50b8e46b3e376fd769c0e18939a48ccc41d20b68
David Hincapie <1508@holbertonschool.com>
2020-03-01 18:53:15 +0000

    Print my name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,14 +1,14 @@
 #include "holberton.h"
 #include <stdio.h>
 /**
- *
- *
- *
- *
+ *main - Fucntion to print the name of itself
+ *@argc: Counter of the elements unused
+ *@argv: String of elements
+ *Return: Zero for succes
  */
-int main(int argc, char *argv[])
+int main(int argc __attribute__((unused)), char *argv[])
 {
 	printf("%s\n", argv[0]);
 
-return(argc);	
+return (0);
 }
