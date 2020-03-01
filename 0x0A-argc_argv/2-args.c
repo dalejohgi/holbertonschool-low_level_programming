ff37a25fb6d17f9ce0e994d3c4161a3c7729b01c
David Hincapie <1508@holbertonschool.com>
2020-03-01 19:45:19 +0000

    Print arguments

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,16 +1,16 @@
 #include "holberton.h"
 #include <stdio.h>
 /**
- *
- *
- * 
- *
+ *main - Print all arguments it receives
+ *@argc: Lenght of argv
+ *@argv: String of elements
+ *Return: 0 if success
  */
 int main(int argc, char *argv[])
 {
 	int i = 0;
 
-	while (*argv[i] != '\0')
+	while (i < argc)
 	{
 		printf("%s\n", argv[i]);
 		i++;
