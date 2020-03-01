e34dcee58712e0f2d9da3164d00abc9f7a6d5545
David Hincapie <1508@holbertonschool.com>
2020-03-01 19:03:46 +0000

    Print the number of arguments

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,15 +1,14 @@
 #include "holberton.h"
 #include <stdio.h>
 /**
- *
- *
- *
- *
+ *main - Prints the number of arguments passed into it
+ *@argc: lenght of argv[]
+ *@argv: String of elements
+ *Return: Zero if success;
  *
  */
-int main(int argc, char *argv[])
+int main(int argc, char *argv[] __attribute__((unused)))
 {
 	printf("%d\n", argc - 1);
-	return (*argv[argc + 1]);
-	
+	return (0);
 }
