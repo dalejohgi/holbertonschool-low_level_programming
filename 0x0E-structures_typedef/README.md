67f56a17db6644a583d592014d411700ce054f50
David Hincapie <1508@holbertonschool.com>
2020-03-10 03:33:18 +0000

    Dog header file

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,1 @@
+Structures and typedef excercisesStructures and typedef excercises
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef DOG
+#define DOG
+/**
+ *struct dog - Data of a dog
+ *@name: Name of the dog
+ *@age: age
+ *@owner: owner
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
