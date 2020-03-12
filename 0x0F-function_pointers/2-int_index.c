c8151ee91483b7792b8ff99087fe4e053e00fd9d
David Hincapie <1508@holbertonschool.com>
2020-03-12 02:57:52 +0000

    Hack

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -12,7 +12,7 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int j, i = 0;
 
-	if (size <= 0 && array != NULL && cmp != NULL)
+	if (size <= 0 || array == NULL || cmp == NULL)
 		return (-1);
 
 	while (i < size)
