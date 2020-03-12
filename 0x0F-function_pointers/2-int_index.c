2dd86b77c808436238e54eb4034f241b85a20df1
David Hincapie <1508@holbertonschool.com>
2020-03-12 02:05:17 +0000

    Look for int

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,27 @@
+#include "function_pointers.h"
+#include <stdlib.h>
+/**
+ *int_index - Look for a coincidence of an element
+ *@array: Array
+ *@size: size of array
+ *@cmp: pointer to function
+ *Return: index of coincidence if success else -1
+ *
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int j, i = 0;
+
+	if (size <= 0 && array != NULL && cmp != NULL)
+		return (-1);
+
+	while (i < size)
+	{
+		j = cmp(array[i]);
+		if (j)
+			return (i);
+
+		i++;
+	}
+return (-1);
+}
