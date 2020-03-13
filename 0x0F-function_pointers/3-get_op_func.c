21c81732a61f140a19586abfd9e17dbe838026a5
David Hincapie <1508@holbertonschool.com>
2020-03-13 04:26:34 +0000

    Calculator

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -2,12 +2,31 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- *
- *
- *
+ *get_op_func - Look for the right operation
+ *@s: operand
+ *Return: Pointer to operation fn if success
  *
  */
 int (*get_op_func(char *s))(int, int)
 {
+		op_t ops[] = {
+	{"+", op_add},
+	{"-", op_sub},
+	{"*", op_mul},
+	{"/", op_div},
+	{"%", op_mod},
+	{NULL, NULL}
+	};
+	int i = 0;
 
+	while (i < 5)
+	{
+		if (ops[i].op[0] == s[0])
+		{
+			return (ops[i].f);
+		}
+	i++;
+	}
+printf("Error\n");
+exit(99);
 }
---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,27 @@
+#include "3-calc.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ *main - Calculator function
+ *@argc: number of elements
+ *@argv: array of elements
+ *Return: 0 if success
+ *
+ */
+int main(int argc, char *argv[])
+{
+	int num1, num2, result;
+	int (*f)(int, int);
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
+	f = get_op_func(argv[2]);
+	result = f(num1, num2);
+	printf("%d\n", result);
+return (0);
+}
