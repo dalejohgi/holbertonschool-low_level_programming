0d803fd5208ecf4a7090d5c849a1987a21cadf5f
David Hincapie <1508@holbertonschool.com>
2020-03-13 21:13:47 +0000

    Print numbs separated by comas

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,32 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+#include <stdarg.h>
+#include <stdlib.h>
+/**
+ *print_numbers - print numbs separated by an string
+ *@separator: separator
+ *@n: Number of numbs
+ *
+ *
+ *
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i, num = 0;
+	va_list numbers;
+
+	if (separator != NULL)
+	{
+		va_start(numbers, n);
+		while (i <= n)
+		{
+			num = va_arg(numbers, int);
+			printf("%d", num);
+			i++;
+			if (i != n + 1)
+				printf("%s", separator);
+		}
+		printf("\n");
+		va_end(numbers);
+	}
+}
