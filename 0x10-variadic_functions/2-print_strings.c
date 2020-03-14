6a70986ae6661621b44be14147d3104a843a4853
David Hincapie <1508@holbertonschool.com>
2020-03-14 18:18:22 +0000

    Printers

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,32 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+/**
+ *print_strings - Print strings
+ *@n: Number of strings
+ *@separator: Separator string
+ *
+ *
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list strings;
+	unsigned int i = 0;
+	char *str;
+
+	if (separator == NULL)
+		separator = "";
+
+	va_start(strings, n);
+	while (i <= n)
+	{
+		str = va_arg(strings, char *);
+		printf("%s", str);
+		if (i < n - 1)
+			printf("%s", separator);
+		i++;
+	}
+	printf("\n");
+	va_end(strings);
+}
---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,48 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+/**
+ *print_all - prints several formats
+ *@format: format to print
+ *
+ *
+ *
+ */
+void print_all(const char * const format, ...)
+{
+	int i = 0;
+	va_list arg;
+	char *separator = "";
+
+	va_start(arg, format);
+	while (format[i] && format)
+	{
+		printf("%s", separator);
+		switch (format[i])
+		{
+			case 'c':
+				printf("%c", va_arg(arg, int));
+				separator = ", ";
+				break;
+			case 's':
+				printf("%s", va_arg(arg, char *));
+				separator = ", ";
+				break;
+			case 'f':
+				printf("%f", va_arg(arg, double));
+				separator = ", ";
+				break;
+			case 'i':
+				printf("%d", va_arg(arg, int));
+				separator = ", ";
+				break;
+			default:
+				separator = "";
+				break;
+		}
+	i++;
+	}
+	va_end(arg);
+printf("\n");
+}
