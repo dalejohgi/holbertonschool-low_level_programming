b24acf759f56dfbe4f9523df7ed12b36a8eaa7ab
David Hincapie <1508@holbertonschool.com>
2020-03-19 23:54:36 +0000

    Fix betty style for list.h

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,4 +1,4 @@
-#ifndef _LIST_H_
+#ifndef _LISTS_H_
 #define _LISTS_H_
 #include <stdlib.h>
 #include <string.h>
@@ -17,9 +17,9 @@
  */
 typedef struct list_s
 {
-    char *str;
-    unsigned int len;
-    struct list_s *next;
+	char *str;
+	unsigned int len;
+	struct list_s *next;
 } list_t;
 size_t print_list(const list_t *h);
 #endif
