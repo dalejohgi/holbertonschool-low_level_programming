a071e0ad388baa2de9c1c8f8f52973e1d3c4a3ad
David Hincapie <1508@holbertonschool.com>
2020-03-22 00:21:37 +0000

    Execute before the main

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include <stdid.h>
+void dofirst(void) __attribute__ ((constructor));
+/**
+ *dofirst - Print a sentence
+ *
+ */
+void dofirst(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
