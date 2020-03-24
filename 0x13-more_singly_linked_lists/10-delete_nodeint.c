340f321b50d9791d05288a80bea0b6b187e907c7
David Hincapie <1508@holbertonschool.com>
2020-03-24 17:20:26 +0000

    Delete a node

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -17,7 +17,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	if (index == 0)
 	{
 		saver = (*head)->next;
-		free(searcher);
+		free(*head);
 		*head = saver;
 		return (1);
 	}
