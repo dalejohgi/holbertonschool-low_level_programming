4f5168d7dd54538875e6672e6b5e084149dd4450
David Hincapie <1508@holbertonschool.com>
2020-03-24 16:24:19 +0000

    Get node

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -12,7 +12,9 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	listint_t *nth;
 
 	if (head == NULL)
-		return(NULL)
+	{
+		return (NULL);
+	}
 
 	nth = head;
 	while (nth->next != NULL && comparator != index)
