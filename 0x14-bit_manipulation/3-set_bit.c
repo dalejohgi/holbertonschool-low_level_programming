5e8600f8cd9811fe83ac7aefc7b39117a12488b7
David Hincapie <1508@holbertonschool.com>
2020-03-27 23:48:33 +0000

    set a bit

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -9,7 +9,7 @@ int set_bit(unsigned long int *n, unsigned int index)
 {
 	unsigned int i = 0;
 
-	if (index > sizeof(unsigned long int) * 8)
+	if (index > sizeof(unsigned long int) * 8 - 1)
 		return (-1);
 
 	while (i < index)
