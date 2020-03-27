88f5c3bf9c7355821820f4a1733c3758819b7483
David Hincapie <1508@holbertonschool.com>
2020-03-27 22:34:17 +0000

    Get a bit

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -9,13 +9,14 @@ int get_bit(unsigned long int n, unsigned int index)
 {
 	unsigned int i = 0;
 
+	if (index > sizeof(n) * 8)
+		return (-1);
+
 	while (i < index)
 	{
 		n >>= 1;
 		i++;
 	}
-	if (n == 0)
-		return (-1);
 	if ((n & 1) == 1)
 		return (1);
 	else
