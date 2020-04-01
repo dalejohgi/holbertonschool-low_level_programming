455e493f6cc3bac1fcadebb72a93c2faf9c18829
David Hincapie <1508@holbertonschool.com>
2020-04-01 02:48:16 +0000

    Fix the code

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,46 +1,41 @@
 #include "holberton.h"
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
 /**
- *read_textfile - Read the text in a file and print it to the stdout
- *@filename: Name of the file to read and write
- *@letters: Number of chars to read
- *Return: 0 if anything fails, else the size of the text
+ *read_textfile - Read and write the text of a file into the std ouput
+ *@filename: Name of the file
+ *@letters: Number of chars to read and write
+ *Return: 0 if fails, size of writing if succes
  *
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fd; /**file descriptor 0 1 2 o -1*/
-	char *buf; /**pointer to where the text is gonna be allocated*/
-	ssize_t size, wrote;
+	int fd;
+	char *buf;
+	ssize_t wrote, readfd;
 
 	if (filename == NULL)
 		return (0);
 
-	fd = open(filename, O_RDONLY);
-
-	if (fd == -1) /**fd is -1 when fails*/
+	fd = open(filename, O_RDWR);
+	if (fd == -1)
 		return (0);
+
 	buf = malloc(sizeof(char) * letters);
 	if (buf == NULL)
-	{
 		return (0);
-	}
-	size = read(fd, buf, letters);
-	if (size == -1)
+
+	readfd = read(fd, buf, letters);
+	if (readfd == -1)
 	{
 		free(buf);
 		return (0);
 	}
-	wrote = write(STDOUT_FILENO, buf, letters);
+	wrote = write(STDOUT_FILENO, buf, readfd);
 	if (wrote == -1)
 	{
 		free(buf);
 		return (0);
 	}
-	close(fd);
 	free(buf);
-	return (size);
+	close(fd);
+	return (readfd);
 }
