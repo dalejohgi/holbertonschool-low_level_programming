7e35999b3d24b3e3eb996678be72965f484135ba
David Hincapie <1508@holbertonschool.com>
2020-05-06 23:44:22 +0000

    Length of a list

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,11 +1,18 @@
 #include "lists.h"
 
 /**
- * dlistint_len -
- *
- * Return: 
+ * dlistint_len - Retunrs the length of a list
+ * @h: pointer to the list
+ * Return: Length
  */
 size_t dlistint_len(const dlistint_t *h)
 {
+	size_t i = 0;
 
+	while (h != NULL)
+	{
+		h = h->next;
+		i++;
+	}
+	return (i);
 }
