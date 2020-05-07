cc5131a1115d69edc647b6d6116f62185cea580f
David Hincapie <1508@holbertonschool.com>
2020-05-07 00:16:59 +0000

    Add node at the beggining

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -1,11 +1,31 @@
 #include "lists.h"
 
 /**
- * add_dnodeint -
- *
- * Return: 
+ * add_dnodeint - Add a node at the beggining of a list
+ *@head: Double pointer to the list
+ *@n: value to add
+ * Return: Pointer to the new node
  */
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
+	dlistint_t *new = malloc(sizeof(dlistint_t));
 
+	if (new == NULL)
+	{
+		return (NULL);
+	}
+	new->n = n;
+	new->prev = *head;
+
+	if (*head == NULL)
+	{
+		*head = new;
+		new->next = NULL;
+	}
+	else
+	{
+		new->next = *head;
+		*head = new;
+	}
+	return (new);
 }
