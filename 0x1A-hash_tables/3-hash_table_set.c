c0b8cc8f502e06a640b3cbf185a948299660bc3d
David Hincapie <1508@holbertonschool.com>
2020-05-29 23:50:59 +0000

    Fix segmentation fault

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -11,26 +11,27 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int k_idx;
 	hash_node_t *tmp;
-	char * updated_value;
+	char *updated_value;
+
 
 	if (!ht || !key || strcmp(key, "") == 0 || !value || !ht->array)
 	return (0);
 	k_idx = key_index((const unsigned char *)key, ht->size);
 	tmp = ht->array[k_idx];
-	while(tmp->next)
+	while (tmp != NULL)
 	{
 		if (strcmp(tmp->key, key) == 0)
-			{
-				updated_value = strdup(value);
-				if (!updated_value)
-					return (0);
-				free(tmp->value);
-				tmp->value = updated_value;
-				return (1);
-			}
+		{
+			updated_value = strdup(value);
+			if (!updated_value)
+				return (0);
+			free(tmp->value);
+			tmp->value = updated_value;
+			return (1);
+		}
 		tmp = tmp->next;
 	}
-	tmp = ht->array[k_idx];	
+	tmp = ht->array[k_idx];
 	ht->array[k_idx] = malloc(sizeof(hash_node_t));
 	if (!ht->array[k_idx])
 	{
