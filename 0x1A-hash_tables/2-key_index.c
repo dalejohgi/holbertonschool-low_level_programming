eccc3ffd70891798d3034ec077c134e376ef5cd6
David Hincapie <1508@holbertonschool.com>
2020-05-29 21:07:11 +0000

    Find an index

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -1,11 +1,12 @@
 #include "hash_tables.h"
 
 /**
- * key_index -
- *
- * Return: 
+ * key_index - Function that using de djb2 gives a nice index for the key
+ * @key: The key
+ * @size: Size of the array of the hash table
+ * Return: An index option for the key
  */
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
-
+	return (hash_djb2(key) % size);
 }
