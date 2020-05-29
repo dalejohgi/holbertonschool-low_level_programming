23ea74a61dff48316982607b307c50d42ff14c00
David Hincapie <1508@holbertonschool.com>
2020-05-29 20:10:04 +0000

    Fix betty style for hash creator

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -16,10 +16,10 @@ hash_table_t *hash_table_create(unsigned long int size)
 		return (NULL);
 	ht->size = size;
 	ht->array = malloc(sizeof(hash_node_t *) * size);
-	if(!ht->array)
+	if (!ht->array)
 	{
 		free(ht);
-		return(NULL);
+		return (NULL);
 	}
 	return (ht);
 }
