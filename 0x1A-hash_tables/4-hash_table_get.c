3a89ae05ff8cc2f96a2c99f955ddf73fd13fd896
David Hincapie <1508@holbertonschool.com>
2020-05-30 00:21:11 +0000

    Get value

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -1,11 +1,25 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_get -
- *
- * Return: 
+ * hash_table_get - Retrieves retrieves a value associated with a key
+ * @ht: Hash table
+ * @key: The key to look for
+ * Return: Value if success else NULL
  */
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
+	hash_node_t *tmp;
+	unsigned long int k_idx;
 
+	k_idx = key_index((const unsigned char *)key, ht->size);
+	tmp = ht->array[k_idx];
+	while (tmp != NULL)
+	{
+		if (strcmp(tmp->key, key) == 0)
+		{
+			return (tmp->value);
+		}
+		tmp = tmp->next;
+	}
+	return (NULL);
 }
