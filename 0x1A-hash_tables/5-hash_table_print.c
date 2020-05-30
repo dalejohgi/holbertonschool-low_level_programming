8b97d60b7c0f6f2f2c3c4cdc41aa9ca69dea1fc0
David Hincapie <1508@holbertonschool.com>
2020-05-30 02:45:15 +0000

    Print a hash table

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -1,11 +1,30 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_print -
- *
- * Return: 
+ * hash_table_print - Prints a hash table
+ * @ht: Hash table
+ * Return: void
  */
 void hash_table_print(const hash_table_t *ht)
 {
+	hash_node_t *tmp = ht->array[0];
+	unsigned int i = 0, coma = 0;
 
+	if (!ht)
+		return;
+	printf("{");
+	while (i < ht->size)
+	{
+		while (tmp != NULL)
+		{
+			if (coma != 0)
+				printf(", ");
+			printf("'%s':'%s'", tmp->key, tmp->value);
+			tmp = tmp->next;
+			coma++;
+		}
+		i++;
+		tmp = ht->array[i];
+	}
+	printf("}\n");
 }
