6922857fde378a64a7965409a0cb13e8716cf407
David <1508@holbertonschool.com>
2020-08-27 23:19:31 +0000

    Add task 0 - 1

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -10,11 +10,39 @@
 
 int binary_search(int *array, size_t size, int value)
 {
-	unsigned int = 0;
-	int *semi_array = array;
+	unsigned int half, low, high, j;
+
 	if (array == NULL)
 	{
 		return (-1);
 	}
-	
+	printf("Searching in array: ");
+	for (j = 0; j < size - 1; j++)
+		printf("%d, ", array[j]);
+	printf("%d\n", array[j]);
+	low = 0;
+	half = (size - 1) / 2;
+	high = size - 1;
+	while (low < high)
+	{
+		if (value < array[half])
+		{
+			high = half - 1;
+			half = (low + high) / 2;
+		}
+		else if (value > array[half])
+		{
+			low = half + 1;
+			half = (low + high) / 2;
+		}
+		else
+		{
+			return (half);
+		}
+		printf("Searching in array: ");
+		for (j = low; j < high; j++)
+			printf("%d, ", array[j]);
+		printf("%d\n", array[j]);
+	}
+	return (-1);
 }
